44270f8090ab3cb7492acf04c070742d805bae51
Aki Vänttinen <aki.vanttinen@sedgestudios.com>
2019-12-15 22:39:52 +0200

    Initial commit for THE HARVESTER

---
+++ ladder_zip.py
@@ -0,0 +1,75 @@
+# Script for creating Ladder Manager compatible Zip archives.
+# 7-Zip command line documentation https://sevenzip.osdn.jp/chm/cmdline/
+
+import os
+import argparse
+import sys
+sys.path.insert(1, "sharpy-sc2")
+from dummy_ladder_zip import create_ladder_zip, LadderZip
+from version import update_version_txt
+
+root_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Files or folders common to all bots.
+common = [
+    (os.path.join("sharpy-sc2", "jsonpickle"), "jsonpickle"),
+    (os.path.join("sharpy-sc2", "sharpy"), "sharpy"),
+    (os.path.join("sharpy-sc2", "python-sc2", "sc2"), "sc2"),
+    (os.path.join("sharpy-sc2", "sc2pathlibp"), "sc2pathlibp"),
+    ("requirements.txt", None),
+    ("version.txt", None),
+    (os.path.join("sharpy-sc2", "config.py"), "config.py"),
+    ("config.ini", None),
+    (os.path.join("sharpy-sc2", "ladder.py"), "ladder.py"),
+    ("ladderbots.json", None),
+]
+
+# Files or folders to be ignored from the archive.
+ignored = [
+    "__pycache__",
+]
+
+zerg_zip = LadderZip("TheHarvester", "Zerg", [
+    ("zergbot", None),
+    (os.path.join("zergbot", "run.py"), "run.py"),
+], common)
+
+
+zip_types = {
+    "harvester": zerg_zip,
+
+    # All
+    "all": None
+}
+
+def get_archive(bot_name: str) -> LadderZip:
+    bot_name = bot_name.lower()
+    return zip_types.get(bot_name)
+
+def main():
+    zip_keys = list(zip_types.keys())
+    parser = argparse.ArgumentParser(
+        description="Create a Ladder Manager ready zip archive for SC2 AI, AI Arena, Probots, ..."
+    )
+    parser.add_argument("-n", "--name", help=f"Bot name: {zip_keys}.")
+    parser.add_argument("-e", "--exe", help="Also make executable (Requires pyinstaller)", action="store_true")
+    args = parser.parse_args()
+
+    bot_name = args.name
+
+    if not os.path.exists('dummy'):
+        os.mkdir('dummy')
+
+    if bot_name == "all" or not bot_name:
+        zip_keys.remove("all")
+        for key in zip_keys:
+            create_ladder_zip(get_archive(key), args.exe)
+    else:
+        if bot_name not in zip_keys:
+            raise ValueError(f'Unknown bot: {bot_name}, allowed values are: {zip_keys}')
+
+        create_ladder_zip(get_archive(bot_name), args.exe)
+
+
+if __name__ == "__main__":
+    main()
---
+++ zergbot/theharvester.py
@@ -0,0 +1,51 @@
+from sharpy.plans.tactics import *
+from sharpy.plans.tactics.zerg import *
+from sharpy.plans.tactics.zone_attack import AttackStatus
+from sc2 import UnitTypeId, Race
+from sc2.ids.upgrade_id import UpgradeId
+
+from sharpy.plans import BuildOrder, Step, SequentialList, StepBuildGas
+from sharpy.plans.acts import *
+from sharpy.plans.acts.zerg import *
+from sharpy.plans.require import *
+
+from sharpy.knowledges import KnowledgeBot
+from sharpy.plans import BuildOrder
+
+
+class HarvesterBot(KnowledgeBot):
+    def __init__(self, build: str = "default"):
+        super().__init__("Harvester")
+        self.distribute = None
+        self.execute_func = None
+        self.conceded = False
+        self.next_action = 0
+
+    async def create_plan(self) -> BuildOrder:
+        self.knowledge.data_manager.set_build("self learning")
+        economy = Step(lambda k: self.next_action == 0, SequentialList([
+                ZergUnit(UnitTypeId.DRONE, 15),
+                ActExpand(2),
+                ZergUnit(UnitTypeId.DRONE, 24),
+                ActExpand(3),
+                ZergUnit(UnitTypeId.DRONE, 38),
+                ActExpand(4),
+                ZergUnit(UnitTypeId.DRONE, 50),
+            ]))
+
+        units = Step(lambda k: self.next_action == 1, SequentialList([
+            ActBuilding(UnitTypeId.SPAWNINGPOOL),
+            ZergUnit(UnitTypeId.ZERGLING, 50),
+        ]))
+
+        return BuildOrder([
+            AutoOverLord(),
+            economy,
+            units
+        ])
+
+    async def on_step(self, iteration):
+        if not self.conceded and self.knowledge.game_analyzer.bean_predicting_defeat_for > 5:
+            await self.chat_send("pineapple")
+            self.conceded = True
+        return await super().on_step(iteration)
