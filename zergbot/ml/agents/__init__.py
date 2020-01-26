15da6015518a4b2f1799558ed611ccc8fdcf3ec7
Aki Vänttinen <aki.vanttinen@sedgestudios.com>
2020-01-26 17:06:00 +0200

    removed old a3cagent from the agents init just to make sure no conflicts happen

--- zergbot/ml/agents/__init__.py
+++ zergbot/ml/agents/__init__.py
@@ -1,5 +1,5 @@
 from .base_ml_agent import BaseMLAgent
 from .random_agent import RandomAgent
 from .semi_scripted_agent import SemiScriptedAgent
-from .a3c_agent import A3CAgent
-from .a3c_agent import ActorCriticModel
+# from .a3c_agent import A3CAgent
+# from .a3c_agent import ActorCriticModel
