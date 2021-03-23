365091560d2d5fa3c89d16f4e7d684343ed6b5b8
lladdy <randallboland@bigpond.com>
2021-03-23 23:24:52 +1030

    Migrate is learning with multiple workers

--- a3c_general_agent/a3c_cartpole_migrate.py
+++ a3c_general_agent/a3c_cartpole_migrate.py
@@ -1,193 +1,108 @@
-from queue import Queue
-from typing import List, Union
+import threading
+
+from common import *
 
 import numpy as np
-import tensorflow as tf
-from tensorflow.python import keras
-from tensorflow.python.keras import layers
-
-from tactics.ml.agents import BaseMLAgent
+import matplotlib.pyplot as plt
 
 
-class ActorCriticModel(keras.Model):
-    def __init__(self, state_size, action_size):
-        super(ActorCriticModel, self).__init__()
-        self.state_size = state_size
-        self.action_size = action_size
-        self.dense1 = layers.Dense(100, activation='relu')
-        self.policy_logits = layers.Dense(action_size)
-        self.dense2 = layers.Dense(100, activation='relu')
-        self.values = layers.Dense(1)
-
-    def call(self, inputs):
-        # Forward pass
-        x = self.dense1(inputs)
-        logits = self.policy_logits(x)
-        v1 = self.dense2(inputs)
-        values = self.values(v1)
-        return logits, values
-
-
-def record(episode,
-           episode_reward,
-           worker_idx,
-           global_ep_reward,
-           result_queue,
-           total_loss,
-           num_steps):
-    """Helper function to store score and print statistics.
-
-    Arguments:
-      episode: Current episode
-      episode_reward: Reward accumulated over the current episode
-      worker_idx: Which thread (worker)
-      global_ep_reward: The moving average of the global reward
-      result_queue: Queue storing the moving average of the scores
-      total_loss: The total loss accumualted over the current episode
-      num_steps: The number of steps the episode took to complete
-    """
-    if global_ep_reward == 0:
-        global_ep_reward = episode_reward
-    else:
-        global_ep_reward = global_ep_reward * 0.99 + episode_reward * 0.01
-    print(
-        f"Episode: {episode} | "
-        f"Moving Average Reward: {int(global_ep_reward)} | "
-        f"Episode Reward: {int(episode_reward)} | "
-        f"Loss: {int(total_loss / float(num_steps) * 1000) / 1000} | "
-        f"Steps: {num_steps} | "
-        f"Worker: {worker_idx}"
-    )
-    result_queue.put(global_ep_reward)
-    return global_ep_reward
-
-
-class Memory:
-    def __init__(self):
-        self.states = []
-        self.actions = []
-        self.rewards = []
-
-    def store(self, state, action, reward):
-        self.states.append(state)
-        self.actions.append(action)
-        self.rewards.append(reward)
-
-    def clear(self):
-        self.states = []
-        self.actions = []
-        self.rewards = []
-
-
-class A3CAgent(BaseMLAgent):
+class Worker(threading.Thread):
     # Set up global variables across different threads
     global_episode = 0
     # Moving average reward
     global_moving_average_reward = 0
     best_score = 0
+    save_lock = threading.Lock()
 
     def __init__(self,
                  state_size,
                  action_size,
-                 learning_rate=0.001,
-                 gamma=0.99):
-        super().__init__(state_size, action_size)
+                 global_model,
+                 opt,
+                 result_queue,
+                 idx,
+                 game_name='CartPole-v0',
+                 save_dir='/tmp'):
+        super(Worker, self).__init__()
+        self.state_size = state_size
+        self.action_size = action_size
+        self.result_queue = result_queue
+        self.global_model = global_model
+        self.opt = opt
         self.local_model = ActorCriticModel(self.state_size, self.action_size)
-        self.mem = Memory()
+        self.worker_idx = idx
+        self.game_name = game_name
+        self.env = gym.make(self.game_name).unwrapped
+        self.save_dir = save_dir
+        self.ep_loss = 0.0
 
-        self.prev_action = None
-        self.prev_state = None
+    def run(self):
+        total_step = 1
+        mem = Memory()
+        while Worker.global_episode < args.max_eps:
+            current_state = self.env.reset()
+            mem.clear()
+            ep_reward = 0.
+            ep_steps = 0
+            self.ep_loss = 0
 
-        self.ep_reward = 0
+            time_count = 0
+            done = False
+            while not done:
+                logits, _ = self.local_model(
+                    tf.convert_to_tensor(current_state[None, :],
+                                         dtype=tf.float32))
+                probs = tf.nn.softmax(logits)
 
-        self.ep_steps = 0
-        self.time_count = 0
-        self.total_step = 0
-        self.gamma = gamma
+                action = np.random.choice(self.action_size, p=probs.numpy()[0])
+                new_state, reward, done, _ = self.env.step(action)
+                if done:
+                    reward = -1
+                ep_reward += reward
+                mem.store(current_state, action, reward)
 
-        # todo: previously these were constructed in the master agent and shared among workers
-        self.opt = tf.train.AdamOptimizer(learning_rate, use_locking=True)
-        self.global_model = ActorCriticModel(self.state_size, self.action_size)  # global network
-        self.global_model(tf.convert_to_tensor(np.random.random((1, self.state_size)), dtype=tf.float32))
-        self.result_queue = Queue()
+                if time_count == args.update_freq or done:
+                    # Calculate gradient wrt to local model. We do so by tracking the
+                    # variables involved in computing the loss by using tf.GradientTape
+                    with tf.GradientTape() as tape:
+                        total_loss = self.compute_loss(done,
+                                                       new_state,
+                                                       mem,
+                                                       args.gamma)
+                    self.ep_loss += total_loss
+                    # Calculate local gradients
+                    grads = tape.gradient(total_loss, self.local_model.trainable_weights)
+                    # Push local gradients to global model
+                    self.opt.apply_gradients(zip(grads,
+                                                 self.global_model.trainable_weights))
+                    # Update local model with new weights
+                    self.local_model.set_weights(self.global_model.get_weights())
 
-        self.ep_loss = 0
+                    mem.clear()
+                    time_count = 0
 
-    def evaluate_prev_action_reward(self, reward: float):
-        if self.prev_action is not None:
-            self.ep_reward += reward
-            self.mem.store(self.prev_state, self.prev_action, reward)
+                    if done:  # done and print information
+                        Worker.global_moving_average_reward = \
+                            record(Worker.global_episode, ep_reward, self.worker_idx,
+                                   Worker.global_moving_average_reward, self.result_queue,
+                                   self.ep_loss, ep_steps)
+                        # We must use a lock to save our model and to print to prevent data races.
+                        if ep_reward > Worker.best_score:
+                            with Worker.save_lock:
+                                print("Saving best model to {}, "
+                                      "episode score: {}".format(self.save_dir, ep_reward))
+                                self.global_model.save_weights(
+                                    os.path.join(self.save_dir,
+                                                 'model_{}.h5'.format(self.game_name))
+                                )
+                                Worker.best_score = ep_reward
+                        Worker.global_episode += 1
+                ep_steps += 1
 
-    def choose_action(self, state: List[Union[float, int]], reward: float):
-        """Choose and return the next action.
-        """
-        self.evaluate_prev_action_reward(reward)
-
-        logits, _ = self.local_model(
-            tf.convert_to_tensor(state[None, :],
-                                 dtype=tf.float32))
-        probs = tf.nn.softmax(logits)
-        self.prev_action = np.random.choice(self.action_size, p=probs.numpy()[0])
-        self.prev_state = state
-
-        self.ep_steps += 1
-
-        return self.prev_action
-
-    def on_end(self, state: List[Union[float, int]], reward: float):
-        self.evaluate_prev_action_reward(reward)
-        # Calculate gradient wrt to local model. We do so by tracking the
-        # variables involved in computing the loss by using tf.GradientTape
-        with tf.GradientTape() as tape:
-            total_loss = self.compute_loss(True,
-                                           state,
-                                           self.mem,
-                                           self.gamma)
-        self.ep_loss += total_loss
-        # Calculate local gradients
-        grads = tape.gradient(total_loss, self.local_model.trainable_weights)
-        # Push local gradients to global model
-        self.opt.apply_gradients(zip(grads,
-                                     self.global_model.trainable_weights))
-        # Update local model with new weights
-        self.local_model.set_weights(self.global_model.get_weights())
-
-        self.mem.clear()
-        self.time_count = 0
-
-        # if done:  # done and print information
-        # Worker.global_moving_average_reward = \
-        #     record(Worker.global_episode, self.ep_reward, 1, #self.worker_idx,
-        #            Worker.global_moving_average_reward, self.result_queue,
-        #            self.ep_loss, ep_steps)
-        A3CAgent.global_moving_average_reward = \
-            record(A3CAgent.global_episode, self.ep_reward, 1,  # self.worker_idx,
-                   A3CAgent.global_moving_average_reward, self.result_queue,
-                   self.ep_loss, self.ep_steps)
-        # We must use a lock to save our model and to print to prevent data races.
-        # if self.ep_reward > Worker.best_score:
-        if self.ep_reward > A3CAgent.best_score:
-            # with Worker.save_lock:
-            print("Saving best model to {}, "
-                  "episode score: {}".format('./data', self.ep_reward))
-            self.global_model.save_weights(
-                os.path.join('./data',
-                             'model_{}.h5'.format('cartpole'))
-            )
-            # Worker.best_score = ep_reward
-            A3CAgent.best_score = self.ep_reward
-        # Worker.global_episode += 1
-        A3CAgent.global_episode += 1
-
-        # todo: this isn't how it originally want.
-        self.prev_action = None
-        self.prev_state = None
-        self.ep_reward = 0
-        self.ep_steps = 0
-        self.time_count = 0
-        self.total_step = 0
-        self.result_queue.empty()
-        self.ep_loss = 0
+                time_count += 1
+                current_state = new_state
+                total_step += 1
+        self.result_queue.put(None)
 
     def compute_loss(self,
                      done,
@@ -219,7 +134,7 @@ class A3CAgent(BaseMLAgent):
 
         # Calculate our policy loss
         policy = tf.nn.softmax(logits)
-        entropy = tf.nn.softmax_cross_entropy_with_logits_v2(labels=policy, logits=logits)
+        entropy = tf.nn.softmax_cross_entropy_with_logits(labels=policy, logits=logits)
 
         policy_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=memory.actions,
                                                                      logits=logits)
@@ -229,15 +144,55 @@ class A3CAgent(BaseMLAgent):
         return total_loss
 
 
-import os
-from tactics.ml.environments.cartpole_env import CartPoleEnv
-
-STOP_FILE: str = "runner-stop.txt"
-
 if __name__ == '__main__':
-    tf.enable_eager_execution()
-    # with tf.device('/cpu:0'):  # use CPU instead of GPU
-    while not os.path.isfile(STOP_FILE):
-        agent = A3CAgent(4, 2)
-        env = CartPoleEnv(agent.choose_action, agent.on_end)
-        env.run()
+    
+    # __init__
+    game_name = 'CartPole-v0'
+    save_dir = args.save_dir
+    save_dir = save_dir
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    env = gym.make(game_name)
+    state_size = env.observation_space.shape[0]
+    action_size = env.action_space.n
+    opt = tf.compat.v1.train.AdamOptimizer(args.lr, use_locking=True)
+    print(state_size, action_size)
+
+    global_model = ActorCriticModel(state_size, action_size)  # global network
+    global_model(tf.convert_to_tensor(np.random.random((1, state_size)), dtype=tf.float32))
+
+    # train
+    if args.algorithm == 'random':
+        random_agent = RandomAgent(game_name, args.max_eps)
+        random_agent.run()
+        exit
+    
+    res_queue = Queue()
+    
+    workers = [Worker(state_size,
+                      action_size,
+                      global_model,
+                      opt, res_queue,
+                      i, game_name=game_name,
+                      save_dir=save_dir) for i in range(args.workers)]
+    
+    for i, worker in enumerate(workers):
+        print("Starting worker {}".format(i))
+        worker.start()
+    
+    moving_average_rewards = []  # record episode reward to plot
+    while True:
+        reward = res_queue.get()
+        if reward is not None:
+            moving_average_rewards.append(reward)
+        else:
+            break
+    [w.join() for w in workers]
+    
+    plt.plot(moving_average_rewards)
+    plt.ylabel('Moving average ep reward')
+    plt.xlabel('Step')
+    plt.savefig(os.path.join(save_dir,
+                             '{} Moving Average.png'.format(game_name)))
+    plt.show()
