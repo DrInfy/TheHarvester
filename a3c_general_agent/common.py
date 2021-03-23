98ec72073a3c4dcfa326578a450a210c37bfba43
lladdy <randallboland@bigpond.com>
2021-03-23 23:16:56 +1030

    Move shared code to common file

---
+++ a3c_general_agent/common.py
@@ -0,0 +1,143 @@
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+import gym
+import multiprocessing
+from queue import Queue
+import argparse
+
+import tensorflow as tf
+from tensorflow.python import keras
+from tensorflow.python.keras import layers
+
+tf.enable_eager_execution()
+
+parser = argparse.ArgumentParser(description='Run A3C algorithm on the game '
+                                             'Cartpole.')
+parser.add_argument('--algorithm', default='a3c', type=str,
+                    help='Choose between \'a3c\' and \'random\'.')
+parser.add_argument('--train', dest='train', action='store_true',
+                    help='Train our model.')
+parser.add_argument('--lr', default=0.001,
+                    help='Learning rate for the shared optimizer.')
+parser.add_argument('--update-freq', default=20, type=int,
+                    help='How often to update the global model.')
+parser.add_argument('--max-eps', default=1000, type=int,
+                    help='Global maximum number of episodes to run.')
+parser.add_argument('--gamma', default=0.99,
+                    help='Discount factor of rewards.')
+parser.add_argument('--save-dir', default='./tmp/', type=str,
+                    help='Directory in which you desire to save the model.')
+parser.add_argument('--workers', default=multiprocessing.cpu_count(), type=int,
+                    help='The number of workers to run.')
+args = parser.parse_args()
+
+
+class ActorCriticModel(keras.Model):
+    def __init__(self, state_size, action_size):
+        super(ActorCriticModel, self).__init__()
+        self.state_size = state_size
+        self.action_size = action_size
+        self.dense1 = layers.Dense(100, activation='relu')
+        self.policy_logits = layers.Dense(action_size)
+        self.dense2 = layers.Dense(100, activation='relu')
+        self.values = layers.Dense(1)
+
+    def call(self, inputs):
+        # Forward pass
+        x = self.dense1(inputs)
+        logits = self.policy_logits(x)
+        v1 = self.dense2(inputs)
+        values = self.values(v1)
+        return logits, values
+
+
+def record(episode,
+           episode_reward,
+           worker_idx,
+           global_ep_reward,
+           result_queue,
+           total_loss,
+           num_steps):
+    """Helper function to store score and print statistics.
+
+    Arguments:
+      episode: Current episode
+      episode_reward: Reward accumulated over the current episode
+      worker_idx: Which thread (worker)
+      global_ep_reward: The moving average of the global reward
+      result_queue: Queue storing the moving average of the scores
+      total_loss: The total loss accumualted over the current episode
+      num_steps: The number of steps the episode took to complete
+    """
+    if global_ep_reward == 0:
+        global_ep_reward = episode_reward
+    else:
+        global_ep_reward = global_ep_reward * 0.99 + episode_reward * 0.01
+    print(
+        f"Episode: {episode} | "
+        f"Moving Average Reward: {int(global_ep_reward)} | "
+        f"Episode Reward: {int(episode_reward)} | "
+        f"Loss: {int(total_loss / float(num_steps) * 1000) / 1000} | "
+        f"Steps: {num_steps} | "
+        f"Worker: {worker_idx}"
+    )
+    result_queue.put(global_ep_reward)
+    return global_ep_reward
+
+
+class RandomAgent:
+    """Random Agent that will play the specified game
+
+      Arguments:
+        env_name: Name of the environment to be played
+        max_eps: Maximum number of episodes to run agent for.
+    """
+
+    def __init__(self, env_name, max_eps):
+        self.env = gym.make(env_name)
+        self.max_episodes = max_eps
+        self.global_moving_average_reward = 0
+        self.res_queue = Queue()
+
+    def run(self):
+        reward_avg = 0
+        for episode in range(self.max_episodes):
+            done = False
+            self.env.reset()
+            reward_sum = 0.0
+            steps = 0
+            while not done:
+                # Sample randomly from the action space and step
+                _, reward, done, _ = self.env.step(self.env.action_space.sample())
+                steps += 1
+                reward_sum += reward
+            # Record statistics
+            self.global_moving_average_reward = record(episode,
+                                                       reward_sum,
+                                                       0,
+                                                       self.global_moving_average_reward,
+                                                       self.res_queue, 0, steps)
+
+            reward_avg += reward_sum
+        final_avg = reward_avg / float(self.max_episodes)
+        print("Average score across {} episodes: {}".format(self.max_episodes, final_avg))
+        return final_avg
+
+
+class Memory:
+    def __init__(self):
+        self.states = []
+        self.actions = []
+        self.rewards = []
+
+    def store(self, state, action, reward):
+        self.states.append(state)
+        self.actions.append(action)
+        self.rewards.append(reward)
+
+    def clear(self):
+        self.states = []
+        self.actions = []
+        self.rewards = []
