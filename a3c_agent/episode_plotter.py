a27cfe1d468c34cc121433bca74bb0796c6be64b
lladdy <randallboland@bigpond.com>
2022-01-14 13:38:02 +1030

    Plot moving averages instead of means

--- a3c_agent/episode_plotter.py
+++ a3c_agent/episode_plotter.py
@@ -46,14 +46,15 @@ def main():
     episodes, rewards, loss = get_log_data(args.model)
     save_plot(args.model, episodes, loss, rewards)
 
-def moving_average(rewards):
-    global_ep_reward = []
-    for reward in rewards:
-        if len(global_ep_reward) == 0:
-            global_ep_reward.append(reward)
+
+def moving_average(series_data):
+    ma = []
+    for data in series_data:
+        if len(ma) == 0:
+            ma.append(data)
         else:
-            global_ep_reward.append(global_ep_reward[-1] * 0.99 + reward * 0.01)
-    return global_ep_reward
+            ma.append(ma[-1] * 0.99 + data * 0.01)
+    return ma
 
 
 def save_plot(model_name, episodes, loss, rewards):
@@ -66,30 +67,17 @@ def save_plot(model_name, episodes, loss, rewards):
     ax1.set_xlabel('Episode')
 
     ax1.set_ylabel('Reward')
-    p1 = ax1.scatter(episodes, rewards, color='red', label="Episode Rewards", s=SCATTER_SIZE)
-    rewards_mean_50 = pd.Series(rewards).rolling(window=10).mean()
-    p2 = ax1.plot(episodes, rewards_mean_50, color='orange', label="Episode Rewards Mean 50", linestyle="-",
-                  linewidth=LINE_WIDTH)[0]
-    rewards_mean_200 = pd.Series(rewards).rolling(window=50).mean()
-    p3 = ax1.plot(episodes, rewards_mean_200, color='yellow', label="Episode Rewards Mean 200", linestyle="--",
-                  linewidth=LINE_WIDTH)[0]
-    ma = moving_average(rewards)
-    p4 = ax1.plot(episodes, ma, color='blue', label="Episode Moving Average", linewidth=LINE_WIDTH)[0]
+    p1 = ax1.scatter(episodes, rewards, color='red', label="Episode Reward", s=SCATTER_SIZE)
+    rewards_ma = moving_average(rewards)
+    p2 = ax1.plot(episodes, rewards_ma, color='red', label="Reward Moving Average", linewidth=LINE_WIDTH)[0]
 
     ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
     ax2.set_ylabel('Loss')  # we already handled the x-label with ax1
-    p5 = ax2.scatter(episodes, loss, label="Episode Loss", s=SCATTER_SIZE)
-    loss_mean_50 = pd.Series(loss).rolling(window=10).mean()
-    p6 = ax2.plot(episodes, loss_mean_50, color='green', label="Episode Loss Mean 50", linestyle="-",
-                  linewidth=LINE_WIDTH)[0]
-    loss_mean_200 = pd.Series(loss).rolling(window=50).mean()
-    p7 = ax2.plot(episodes, loss_mean_200, color='purple', label="Episode Loss Mean 200", linestyle="--",
-                  linewidth=LINE_WIDTH)[0]
-    # ax2.legend(loc='center left')
-    # ax2.tick_params(axis='y')
-    plt.legend([p1, p2, p3, p4, p5, p6, p7],
-               ["Episode Rewards", "Episode Rewards Mean 50", "Episode Rewards Mean 200", "Episode Moving Average",
-                "Episode Loss", "Episode Loss Mean 50", "Episode Loss Mean 200", ])
+    p3 = ax2.scatter(episodes, loss, color='blue', label="Episode Loss", s=SCATTER_SIZE)
+    loss_ma = moving_average(loss)
+    p4 = ax2.plot(episodes, loss_ma, color='blue', label="Loss Moving Average", linewidth=LINE_WIDTH)[0]
+    plt.legend([p1, p2, p3, p4],
+               ["Episode Rewards", "Reward Moving Average", "Episode Loss", "Loss Moving Average",])
     plt.savefig(model_paths.PLOT_FILE_PATH, dpi=300)
     plt.show()
 
